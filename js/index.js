7facc298d7705aedfa5c9a8afc6350a225604555
harshit <harshitbakraniya@gmail.com>
2022-04-17 12:04:06 +0530

    Add some page and update design

--- js/index.js
+++ js/index.js
@@ -1,28 +1,23 @@
 let bars = document.getElementById("bars");
-let close1 = document.getElementById("close");
+let close = document.getElementById("close");
 let nav_links = document.getElementById("nav-links");
 
 bars.addEventListener("click", () => {
-    nav_links.style.display = "block";
-    close1.style.display = "block";
-    bars.style.display = "none"
+    nav_links.classList.add("active");
 })
-
-close1.addEventListener("click", () => {
-    nav_links.style.display = "none";
-    close1.style.display = "none";
-    bars.style.display = "block"
+close.addEventListener("click", () => {
+    nav_links.classList.remove("active");
 })
 
 window.addEventListener("scroll", () => {
     let nav = document.getElementById("navbar");
     if (window.pageYOffset > 5) {
-        nav.style.background = "white";
+        // nav.style.background = "white";
         // nav.style.boxShadow = "0 0 7px 0 rgba(0,0,0,0.1)";
-        nav.style.border = "5px solid #06d6a0";
+        // nav.style.border = "5px solid #06d6a0";
     } else {
-        nav.style.background = "transparent";
+        // nav.style.background = "transparent";
         // nav.style.boxShadow = "none";    
-        nav.style.border = "5px solid #ffffff";
+        // nav.style.border = "5px solid #ffffff";
     }
 })
